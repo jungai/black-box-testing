12c3e2b4a1a0a72aa3155570284e186dd14ba7e6
jungai <juworaphol125@gmail.com>
2023-05-20 22:12:22 +0700

    refactor: more cancel request

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,22 @@
 <script lang="ts" setup>
-const signal = ref<AbortController | null>();
+const controller = ref<AbortController | null>();
 const apiUrl = "https://jsonplaceholder.typicode.com/todos/1";
 const item = ref();
 const isFinished = ref<boolean>(false);
 const count = ref<number>(0);
 
 onMounted(async () => {
-  signal.value = new AbortController();
-  const rawResp = await fetch(apiUrl, { signal: signal.value.signal });
-  const resp = await rawResp.json();
-  item.value = resp;
-  isFinished.value = true;
+  controller.value = new AbortController();
+  setTimeout(async () => {
+    const rawResp = await fetch(apiUrl, { signal: controller.value!.signal });
+    const resp = await rawResp.json();
+    item.value = resp;
+    isFinished.value = true;
+  }, 2000);
 });
 
 onUnmounted(() => {
-  signal.value?.abort();
+  controller.value?.abort();
 });
 </script>
 
@@ -24,5 +26,6 @@ onUnmounted(() => {
     <h1 v-else>Hello {{ item.title }}</h1>
     <p>count -> {{ count }}</p>
     <button @click="() => count++">click me</button>
+    <button @click="() => controller?.abort()">cancel</button>
   </div>
 </template>
